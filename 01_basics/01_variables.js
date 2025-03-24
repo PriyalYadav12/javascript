61f52d1c02ea1b0136f15e15c6d433a87d530920
Priyal yadav <167217975+PriyalYadav12@users.noreply.github.com>
2025-03-24 20:48:03 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =245555
+let accountEmail = "hitesh12@gmail.com"
+var accountPassword ="12334"
+accountCity = "jaipur"
+
+//accountId =2 not allowed
+accountEmail = "jdhgkuwwgb@gmail.com"
+accountPassword ="121121"
+accountCity ="beneguluru"
+let accounttState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accounttState])
+/*Prefer not to use var
+because of issues in block scope and functional scope
+*/
