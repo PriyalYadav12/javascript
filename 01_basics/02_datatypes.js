7e254d6373fa5f5dd25acfbacf8e76be807c8b00
Priyal yadav <167217975+PriyalYadav12@users.noreply.github.com>
2025-03-24 21:23:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+// code readability should be higher
+console.log("hitesh")
+
+
+let name = "hitesh"
+let age =17
+let isLoggedIn = false
+let state;
+
+/* number -> 2 to power 53
+bigint
+string -> ""
+boolean -> false true
+null -> standalone value
+undefined -> 
+symbol -> unique
+
+
+
+OBJECT :
+
+ */
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
